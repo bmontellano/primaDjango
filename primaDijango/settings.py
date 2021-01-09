b95f610b5b47ddf0ef716054313ab46b29d4d643
Brian Elias Montellano <bmontellano@gmail.com>
2021-01-08 22:40:02 -0800

    connected mongoDB using djongo

--- primaDijango/settings.py
+++ primaDijango/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'primaDijango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'djongo',
+         'NAME': 'prima_dijango',
+     }
+ }
+
+
 
 
 # Password validation
